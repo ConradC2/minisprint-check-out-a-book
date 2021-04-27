27fd5924c363fc5e65ba2aeb40ca014b872e0777
Gordon Deng <gzd2032@gmail.com>
2021-04-28 00:00:26 +0200

    end of day.

---
+++ client/cypress/integration/examples/index.spec.js
@@ -0,0 +1,11 @@
+describe('Test of index', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+ 
+  it('Should display SDI Library', () =>{
+    should.('header' /SDI Library/)
+    expect.('heading').getby
+  })
+
+})
